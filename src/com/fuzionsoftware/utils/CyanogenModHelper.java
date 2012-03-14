efea47f8d616ceeccfc4fe6a34f0d662a441afc2
Ryan Welton <rwelton24@gmail.com>
2012-03-14 13:58:22 -0400

    removed uneeded import

--- src/com/fuzionsoftware/utils/CyanogenModHelper.java
+++ src/com/fuzionsoftware/utils/CyanogenModHelper.java
@@ -6,7 +6,6 @@ import java.io.InputStreamReader;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
